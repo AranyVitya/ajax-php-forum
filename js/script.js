a33b3b7434516520402428e6e7c958b5feb7c76d
Vitya Arany <aranyviktor@Aranys-MacBook.local>
2017-06-09 11:35:38 +0200

    date content
    date content

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(function() {
 	//GET/READ -- az oldal első betöltésekor kell, hogy megjelenjenek a már beírt üzenetek
 	// pollingal megoldott ajax GET kérés
 	// setInterval-al megvalosított polling
-	setInterval(function() {
+	//setInterval(function() {
 		$.ajax({
 			url: 'php/messages_get.php',
 			dataType: 'JSON',	//dataType-ot akkor használunk ha a szevertől várjuk az adatot
@@ -12,11 +12,21 @@ $(function() {
 				$('#default-panel').html('');		//ha ez nem lenne itt akkor a setTimeout által beállított 3 mp enkénti GET kérésre kapott választ minden 3. másodpercben kiírná egymás alá			
 
 				for(var i = 0; i <= response_get.length-1; i++){
-					$('#default-panel').append('<div class="panel panel-default"><div class="panel-heading" id="name-content">' + response_get[i].username + '</div><div class="panel-body" id="message-content">' + response_get[i].messages + '</div></div>');
-					}
+					$('#default-panel').append('<div class="panel panel-default">\
+        											<div class="panel-heading" id="name-content">\
+          												<div class="container-fluid">\
+              													<div class="row">\
+              														<div class="col-xs-4" id="name-content">' + response_get[i].username + '</div>\
+              														<div class="col-xs-8" id="date-content">' + response_get[i].currentdate + '</div>\
+              													</div>\
+          												</div>\
+        											</div>\
+        											<div class="panel-body" id="message-content">' + response_get[i].messages + '</div>\
+    											</div>');		
+				}
 			},
 		});
-	},3000);	//3 másodpercenként végrehajtja a setInterval-on belül leírt kódot
+	//},3000);	//3 másodpercenként végrehajtja a setInterval-on belül leírt kódot
 	
 	//POST/CREAT
 	$('#create-form').on('submit', function(event) {
--- php/messages_post.php
+++ php/messages_post.php
@@ -4,13 +4,21 @@ require "conf_connection.php";
 
 $name = $_POST['username'];
 $messages = $_POST['messages'];
+$date = date("Y-m-d h:i:s");
 
-echo   "<div class=\"panel panel-default\">
-			<div class=\"panel-heading\" id=\"name-content\">". $name ."</div>
+echo    "<div class=\"panel panel-default\">
+        	<div class=\"panel-heading\" id=\"name-content\">
+          		<div class=\"container-fluid\">
+            		<div class=\"row\">
+              			<div class=\"col-xs-4\" id=\"name-content\">". $name ."</div>
+              			<div class=\"col-xs-8\" id=\"date-content\">". $date ."</div>
+            		</div>
+          		</div>
+        	</div>
         	<div class=\"panel-body\" id=\"message-content\">". $messages ."</div>
-        </div>";
+    	</div>";
 
-$sql = "INSERT INTO messages (username, messages) VALUES ('$name', '$messages')";
+$sql = "INSERT INTO messages (username, messages, currentdate) VALUES ('$name', '$messages', NOW())";
 
 if ($conn->query($sql) === TRUE) {
 	//echo "New record created successfully";
